75cd8ed0efc9a1d2012d4385192f39362fc09397
Daria Naumova <dariagnaumova@gmail.com>
2019-12-29 21:22:32 -0500

    questions for the quiz populated

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ var questions = [
         answer: "parentheses"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "Which of the following jQuery method sets attributes of an element?",
+        choices: ["attr(name, value)", " setAttr(name, value)", "setAttributes(name, value)", "none of the above"],
+        answer: "attr(name, value)"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "Which of the following bootstrap style is to be used if you want to create a .navbar that scrolls with the page?.",
+        choices: [".navbar-static-top", " .navbar-fixed", ".navbar-fixed-top", "none of the above"],
+        answer: ".navbar-static-top"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "Which of the following function of Array object returns a string representing the array and its elements?",
+        choices: ["toSource()", "sort()", "splice()", "toString()"],
+        answer: "toString()"
     },
   ];
 
