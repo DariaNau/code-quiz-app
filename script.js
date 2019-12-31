c7b94b6a5950805767f76a88aa28f2e50cac8ea5
Daria Naumova <dariagnaumova@gmail.com>
2019-12-30 21:26:42 -0500

    timer is set

--- script.js
+++ script.js
@@ -28,25 +28,41 @@ var questions = [
     },
   ];
 
+  var secondsLeft = (questions.length) * 15;
+
   $(document).ready(function() {
 
     $('.start').click(function(e){
           e.preventDefault();
+          setTimer();
           $('.start').hide();
           $('.quiz').show();
           showQuestion();
       });
 
-      //clicking on the start quiz button hides the start page and shows the quiz page while firing the showQuestion() function
+      function setTimer() {
+          
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+    $('#timer').html(secondsLeft);
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      scores();
+    }}, 1000);
+}
+
+
+
+//clicking on the start quiz button hides the start page and shows the quiz page while firing the showQuestion() function
       
-      function showQuestion(){
-        var choices = questions[currentQuestion].choices;
-        var question = questions[currentQuestion].title;
-        $('.quiz h2').text(question);
-        $('.quiz ol').html('');
-        for (var i=0; i<parseInt(choices.length); i++){
-        var show = questions[currentQuestion].choices[i];
-        $('.quiz ol').append(`<li class="button-select" id="${i}">${show}</li>`);
+    function showQuestion(){
+    var choices = questions[currentQuestion].choices;
+    var question = questions[currentQuestion].title;
+    $('.quiz h2').text(question);
+    $('.quiz ol').html('');
+    for (var i=0; i<parseInt(choices.length); i++){
+    var show = questions[currentQuestion].choices[i];
+    $('.quiz ol').append(`<li class="button-select" id="${i}">${show}</li>`);
     }
 
     $("li").click(function() {
@@ -59,14 +75,29 @@ var questions = [
     $('cite').html('<h4>Correct!<h4>').fadeOut(600);
     currentQuestion ++;
     showQuestion();
-    
+
+ // highlight timer in css later! 
+ 
     } else{
     $('cite').fadeIn(100);
     $('cite').html('<h4>Wrong!<h4>').fadeOut(600);
+    secondsLeft = secondsLeft-10;
     currentQuestion ++;
     showQuestion();
 }
 });
+
+
+// function scores() {
+//     timeEl.textContent = " ";
+  
+//     var imgEl = document.createElement("img");
+  
+//     imgEl.setAttribute("src", "images/image_1.jpg");
+//     mainEl.appendChild(imgEl);
+  
+//   }
+
 
 
 }});
